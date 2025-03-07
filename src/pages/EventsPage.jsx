0f20179b4358b8e7dd5a6bb5e023c14df9b491f1
IvnCr <icr0015@alu.medac.es>
2025-03-07 12:24:42 +0100

    arreglo fondo de boton

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -48,10 +48,11 @@ function EventsPage() {
                             <p>{event.location}</p>
                             <Button
                                 onClick={() => handleToggleEvent(event)}
-                                className={`mt-4 ${isUserEvent ? 'bg-red-600 text-white border border-white' : 'bg-green-700 text-white border border-white'}`}
+                                className={`mt-4 ${isUserEvent ? 'bg-red-500 text-white border border-white' : 'bg-green-600 text-white border  border-white'}`}
                             >
                                 {isUserEvent ? 'Cancelar participación' : 'Apuntarme'}
                             </Button>
+
                         </div>
                     );
                 })}
